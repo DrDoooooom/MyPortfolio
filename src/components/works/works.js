5fe60754681a75e17e9e495deee021ca8811bd41
Swapnil Kharat <swapnil200229@gmail.com>
2023-10-21 21:15:55 +0530

    Changed some CSS for Works and changed Font

--- src/components/works/works.js
+++ src/components/works/works.js
@@ -1,33 +1,26 @@
 import React from "react";
 
 import './works.css';
-// import portfolio2 from '../../assets/portfolio-2.jpeg';
+import project from'../../assets/projects.gif'
 
 const Works = () => {
     return (
-        <section id="works">
-            <h2 className="workstitle">My Portfolio</h2>
-            <span className="worksDec">
-                {" "}
-                Coding my way to the future, one project at a time.{" "}
-            </span>
-            
-            <div className="worksImgs">
-                <div className="project1">
-                    {/* <img src={portfolio2} alt="" className="WorksImg" /> */}
-                    <div className="wrapper">
-                        <div className="neon-wrapper">
-                            <div className="neon-text">
-                                PROJECTS
-                            </div>
-                        </div>
-                    </div>
-                    <p className="projectInfo1">
-                        Currently working on biulding a E-Commerce Website
-                    </p>
-                </div>
-            </div>
-        </section>
+      <section id="works">
+        <h2 className="workstitle">My Portfolio</h2>
+        <span className="worksDec">
+          {" "}
+          Coding my way to the future, one project at a time.{" "}
+        </span>
+
+        <div className="worksImgs">
+          <div className="project1">
+            <img src={project} alt="" className="WorksImg" />
+            <p className="projectInfo1">
+              Currently working on building a E-Comm Website
+            </p>
+          </div>
+        </div>
+      </section>
     );
 };
 
